4b7d8be2d98586c73972f647ede578bf68541bb2
Renato Cardoso <renatoscardoso77@gmail.com>
2024-12-01 11:12:03 -0400

    feat: add server response for group diet

--- handler/response.go
+++ handler/response.go
@@ -103,3 +103,29 @@ type UpdateIndividualDietResponse struct {
 	Message string `json:"message"`
 	Data 	schemas.IndividualDiet `json:"data"`
 }
+
+// GroupDiet
+type CreateGroupDietResponse struct {
+	Message string `json:"message"`
+	Data 	schemas.GroupDiet `json:"data"`
+}
+
+type DeleteGroupDietResponse struct {
+	Message string `json:"message"`
+	Data 	schemas.GroupDiet `json:"data"`
+}
+
+type ShowGroupDietResponse struct {
+	Message string `json:"message"`
+	Data 	schemas.GroupDiet `json:"data"`
+}
+
+type ListGroupDietsResponse struct {
+	Message string `json:"message"`
+	Data 	[]schemas.GroupDiet `json:"data"`
+}
+
+type UpdateGroupDietResponse struct {
+	Message string `json:"message"`
+	Data 	schemas.GroupDiet `json:"data"`
+}
